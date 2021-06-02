23e1bc8da08cb2edca79dd278b06cbabb22d9fdf
bobofishstix <tynerbrad0313@gmail.com>
2021-06-02 18:24:18 -0500

    Added parameters

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,9 +1,41 @@
+import * as React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <p>Hello</p>
-  );
+
+export default class GetPokemon extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        pokemonName: null,
+        // pokemonID: null,
+        // pokemonType: null,
+        // pokemonImage: null
+    };
+  }
+  async componentDidMount() {
+      // GET request using axios with error handling
+          axios.get('https://pokeapi.co/api/v2/pokemon/1/')
+          .then(response => this.setState({ pokemonName: response.data.name }))
+          .catch(error => {
+              // this.setState({ errorMessage: error.message });
+              console.error('There was an error!', error);
+          });
+  }
+    // return (
+    //   console.log(response)
+    // );
+  
+  render() {
+    return (
+      <div className='App'>
+        <p> Name:{this.state.pokemonName} </p>
+        {/* <p> ID:{pokemonID} </p> */}
+        {/* <p> Type:{pokemonType} </p> */}
+      </div>
+    )
+
+  }
 }
 
-export default App;
